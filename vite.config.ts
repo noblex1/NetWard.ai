95b27e8f1a0b42c2d5cd498ccac63b44d3d20c73
noblex1 <mrgem156@gmail.com>
2025-07-02 23:59:18 +0000

    Fix vite.config.ts: coonditional base for local and Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/NetWard.ai/', // <-- Add this line
+  base: mode === "production" ? "/NetWard.ai/" : "/",
   server: {
     host: "::",
     port: 8080,
